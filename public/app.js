600dcaab3615adb281eb6d19ee35459dd50d5073
Ibrahim Tin <ibrahimtin0786@gmail.com>
2024-05-27 13:48:10 +0530

    status feature is updated

---
+++ public/app.js
@@ -0,0 +1,12 @@
+let btns = document.getElementsByClassName('helper');
+for(let index=0;index<btns.length;index++)
+    {
+        btns[index].addEventListener('click',()=>{
+            let rote = btns[index].children[0].children[0];
+            rote.classList.add('rotate')
+            setTimeout(()=>{
+                rote.classList.remove("rotate");
+                rote.classList.add('rm');
+            },2500);
+        });
+    }
